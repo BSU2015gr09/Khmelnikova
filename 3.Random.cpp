1170a56f215a833d1fb9127b7860ef524c89b84d
barvenov <barvenovsa@gmail.com>
2015-10-27 11:15:59 +0400

    Замечания по коду...

--- 3.Random.cpp
+++ 3.Random.cpp
@@ -7,7 +7,7 @@ using std:: cin;
 int main(){
 	setlocale(LC_ALL,"Russian");
 int a=4096, c=150889, m=714025, i=0;
-int mas[21];
+int mas[21]; // зачем массив????
 mas[0]=1;
 for (i; i<22; i++){
 	mas[i+1]=(a*mas[i] + c)%m;
