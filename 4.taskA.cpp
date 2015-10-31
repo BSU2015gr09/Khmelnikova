c316dd91e1d2238afbb2486aa6100c2eaead0458
Khmelnikova Nadya <obeya666@gmail.com>
2015-10-31 17:40:35 +0300

    Homework №4

---
+++ 4.taskA.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>/*алгоритм XORShift генерации псевдослучайных чисел с использованием XOR и битовыми сдвигами*/
+using std:: cout;
+void xorshift(int &x){
+	x=10;
+	int tmp;
+		tmp ^= (x << 21);
+		tmp ^= (tmp >> 34);
+		x ^= (tmp << 4);
+		cout << x << "\n";
+}
+int main(){
+	int x;
+	xorshift(x);
+}
+
+
+
