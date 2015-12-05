6110f36d2edb0f21fd8a06c6b47708cf9d6c15ec
Khmelnikova Nadya <obeya666@gmail.com>
2015-12-05 17:07:06 +0300

    Homework №6

---
+++ 6taskG.cpp
@@ -0,0 +1,43 @@
+﻿/*В программе объявлен массив А элементов типа char (размер массива 20). 
+Пользователь вводит массив c клавиатуры. 
+Затем заменяем последнюю встреченную в массиве букву Z на букву F.*/
+#include <iostream>
+#include <clocale>
+using namespace std;
+void mas (int const N, char A[]) {
+	for (int i=0; i<N; i++) {
+		cout << "Введите элемент массива " << "\n";
+		cin >> A[i];
+	}
+}
+void output (int const N, char A[]) {
+	for (int i=0; i<N; i++) {
+		cout << A[i] << "  ";
+	}
+}
+void exch (int const N, char A[]) {
+	int k=0;
+	for (int i=N-1; i>0; i--) {
+		if (A[i] == 'Z') {
+			A[i] = 'F';
+			k++;
+			break;
+		}
+	}
+	cout << "\n\n";
+	if (!k) {
+	cout << "В массиве нет символа 'Z' " << "\n";}
+	else
+	output(N, A);
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int const N = 20;
+	char A[N];
+	mas(N, A); // Заполнение массива с клавиатуры
+	cout << "Массив:  " << "\n";
+	output(N, A); // Вывод массива
+
+	exch(N, A); // Замена последней встреченной в массиве буквы Z на букву F
+}
---
+++ 6taskV.cpp
@@ -0,0 +1,46 @@
+﻿/*В программе объявлен массив А элементов типа char (размер массива 20). Пользователь вводит массив c клавиатуры. 
+Затем ищем в массиве какая буква встречается чаще всего. Ее выводим на экран.*/
+#include <iostream>
+#include <clocale>
+using namespace std;
+void mas (int const N, char A[]) {
+	for (int i=0; i<N; i++) {
+		cout << "Введите элемент массива " << "\n";
+		cin >> A[i];
+	}
+}
+void output (int const N, char A[]) {
+	for (int i=0; i<N; i++) {
+		cout << A[i] << "  ";
+	}
+}
+void element(int const N, char A[]) {
+	int  maxcount=1, num=0;
+	int count = 0;
+	for (int i=0; i<N; i++) {
+		for (int j=0; j<N; j++) {
+			if (A[i] == A[j] && i!=j) {
+				count++;
+			}
+			if (count > maxcount) {
+				maxcount = count;
+				count = 0;
+				num = i;
+			}
+		}
+	}
+	if (num > 0) {
+		cout << "\n" << "Чаще всего встречается буква " << A[num] << "\n" ;
+	}
+	else 
+		cout << "\n" << "В массиве нет повторяющихся элементов " << "\n";
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int const N = 20;
+	char A[N];
+	mas(N, A); // Заполнение массива с клавиатуры
+	cout << "Массив:  " << "\n";
+	output(N, A); // Вывод массива
+	element(N, A);// Ищем в массиве какая буква встречается чаще всего
+}
