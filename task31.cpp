a9039107410d7b166873769128a1794669bdd363
Khmelnikova Nadezhda <nkhmelnikova@gmail.com>
2016-02-28 18:32:37 +0300

    add task31

---
+++ task31.cpp
@@ -0,0 +1,9 @@
+﻿/*31.(7-8)В массиве А(N) встречаются лишь числа от 1 до 20. Упорядочить элементы массива в порядке частоты встречаемости чисел.*/
+#include <iostream>
+#include <clocale>
+#include <ctime>
+using namespace std;
+int main()
+{
+
+}
